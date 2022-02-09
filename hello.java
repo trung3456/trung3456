ab717f7c7dba200b23b4c64e6732a678c03125a3
Công Trung Trần <mikuyoshida707@gmail.com>
2022-02-10 06:44:40 +0700

    f

--- hello.java
+++ hello.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 class hello {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Hello, World!");
     }
 }
