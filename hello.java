89b2b66a78c4877bd74c7f2b0b9c92278fa8cb4d
Công Trung Trần <mikuyoshida707@gmail.com>
2022-02-10 06:33:32 +0700

    hello

--- hello.java
+++ hello.java
@@ -4,13 +4,5 @@ class hello {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int number = scanner.nextInt();
-        while (number <=10) {
-            System.out.println(number);
-            number++;
-            if (number == 5) {
-                break;
-            }
-        }
     }
 }
