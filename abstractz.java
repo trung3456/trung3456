ec89ecda6586a3ad33ccc83919773bfd5dcb9998
Công Trung Trần <mikuyoshida707@gmail.com>
2022-02-12 06:58:08 +0700

    12/02

--- abstractz.java
+++ abstractz.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 // create abstract class and abstract method "nameOfAnimal"
 abstract class Animal{
     public abstract void nameOfAnimal();
@@ -12,8 +14,19 @@ class Chicken extends Animal {
 
 class abstractz {
     public static void main(String[] args) {
+        // Check type of variable
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.nextLine();
+        // System.out.println(name.getClass().getSimpleName());
+
         // create Oj
         Chicken ga = new Chicken();
         ga.nameOfAnimal();
+
+        hello t2 = new hello();
+        t2.seta(5);
+        t2.setName("Trungpro");
+        System.out.println(t2.geta());
+        System.out.println(t2.getName());
     }
 }
